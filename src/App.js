3ca3652e380d17fb5765283eb26ee5b12b599da6
Herry Vinh <hongvinhxc@gmail.com>
2019-05-20 13:22:28 +0700

    Add input -Add a new item-

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      value : "",
       todoItems :[
       {title: "Go to school", isCompleted : true},
       {title: "Go to work", isCompleted : false},
       {title: "Go out with friend", isCompleted : false}
     ]};
+
+    this.onKeyUp = this.onKeyUp.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
   onItemClick(item) {
     return (even) => {
@@ -26,9 +30,37 @@ class App extends Component {
       })
     };
   }
+
+  onKeyUp(even) {
+    if (even.keyCode === 13) {
+      let value = even.target.value
+      if (!value) {
+        return;
+      }
+      value = value.trim(); 
+      if (!value) {
+        return;
+      }
+      this.setState({
+        value: '',
+        todoItems :[
+        {title: value, isCompleted: false},
+        ...this.state.todoItems
+        ]
+      })
+    }
+  }
+
+  onChange(even) {
+    this.setState({
+      value: even.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
+        <input type="text" onKeyUp={this.onKeyUp} placeholder="Add a new item" value={this.state.value} onChange={this.onChange} />
         {
          this.state.todoItems.length > 0 && this.state.todoItems.map((item, index) => <TodoItem key={index} item={item} onClicked={this.onItemClick(item)}/>)
         }
